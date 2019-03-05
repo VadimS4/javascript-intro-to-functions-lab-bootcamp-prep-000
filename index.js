d843d9dc22065aa74b4d51bb62b6b4589bf4a2f5
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-03-05 02:36:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string)
 {
   var cantAnswer = "I can't hear you!";
   var Answer = "YES INDEED!";
-  var yesAnswer = "I love you, too!";
+  var yesAnswer = "I love you, too.";
   
   if (string.toLowerCase(string) === string)
   {
